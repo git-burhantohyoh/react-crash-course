af6992efc7e58d4f884b84ab8f6402f7651f71c3
BURHAN TOHYOH <burhantohyoh.work@gmail.com>
2020-09-14 22:38:45 +0700

    22.40 install react and create Navbar, Input_field

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from './Navbar.js';
+import Input from './Input.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
